15b7d8f3978363dffac8a168b9cdc9be45a93e23
Chuan Chen <riverchen@gmail.com>
2017-02-09 23:25:10 -0500

    Added buffer to render graphics.

--- PuzzleFighter/PuzzleFighterGame.cs
+++ PuzzleFighter/PuzzleFighterGame.cs
@@ -14,10 +14,12 @@ namespace PuzzleFighter {
 		Graphics g;
 		int xSize = 6;
 		int ySize = 15;
+		Bitmap Backbuffer;
 		public PuzzleFighterGame() {
             InitializeComponent();
             this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
-
+			this.Load += new EventHandler(Form1_CreateBackBuffer);
+			this.Paint += new PaintEventHandler(Form1_Paint);
 			b = new Board(xSize, ySize);
 			b.grid[0, 0] = new Block(0, 0);
 			b.grid[0, 1] = new Block(0, 1);
@@ -40,28 +42,41 @@ namespace PuzzleFighter {
         void Form1_KeyPress(object sender, KeyPressEventArgs e) {
             Console.WriteLine(e.KeyChar);
         }
-		private void draw() {
-			g = this.CreateGraphics();
-			g.Clear(Color.Black);
-			SolidBrush brush;
-			Pen p = new Pen(Color.White, 1);
-			for (int i = 0; i < xSize; i++) {
-				for (int j = 0; j < ySize; j++) {
-					g.DrawRectangle(p, new Rectangle(10 * i, 10 * j, 10, 10));
-					if (b.grid[i, j] != null) {
-						brush = new SolidBrush(Color.FromName(b.grid[i, j].color.ToString()));
-						if (b.grid[i, j].type == BlockType.Normal) {
-							g.FillRectangle(brush, new Rectangle(10 * i, 10 * j, 10, 10));
-						} else if (b.grid[i, j].type == BlockType.Clear) {
-							g.FillEllipse(brush, new Rectangle(10 * i, 10 * j, 10, 10));
+		void Form1_Paint(object sender, PaintEventArgs e) {
+			if (Backbuffer != null) {
+				e.Graphics.DrawImageUnscaled(Backbuffer, Point.Empty);
+			}
+		}
+		void Form1_CreateBackBuffer(object sender, EventArgs e) {
+			if (Backbuffer != null) {
+				Backbuffer.Dispose();
+			}
+			Backbuffer = new Bitmap(ClientSize.Width, ClientSize.Height);
+		}
+		void draw() {
+			if (Backbuffer != null) {
+				using (var g = Graphics.FromImage(Backbuffer)) {
+					g.Clear(Color.Black);
+					SolidBrush brush;
+					Pen p = new Pen(Color.White, 1);
+					for (int i = 0; i < xSize; i++) {
+						for (int j = 0; j < ySize; j++) {
+							g.DrawRectangle(p, new Rectangle(10 * i, 10 * j, 10, 10));
+							if (b.grid[i, j] != null) {
+								brush = new SolidBrush(Color.FromName(b.grid[i, j].color.ToString()));
+								if (b.grid[i, j].type == BlockType.Normal) {
+									g.FillRectangle(brush, new Rectangle(10 * i, 10 * j, 10, 10));
+								} else if (b.grid[i, j].type == BlockType.Clear) {
+									g.FillEllipse(brush, new Rectangle(10 * i, 10 * j, 10, 10));
+								}
+								brush.Dispose();
+							}
 						}
-						brush.Dispose();
 					}
 				}
+				Invalidate();
 			}
-			g.Dispose();
 		}
-
 		private void button1_Click(object sender, EventArgs e) {
 			draw();
 			b.dropBlocks();
